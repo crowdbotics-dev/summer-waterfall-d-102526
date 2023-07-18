9a2089e3a2f10c788774fc88f86b42542801ce6c
Crowdbotics <engineering@crowdbotics.com>
2023-07-18 10:22:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "summer-waterfall-d-102526",
+  url: "https://summer-waterfall-d-102526.botics.co",
+  api: "https://summer-waterfall-d-102526.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
